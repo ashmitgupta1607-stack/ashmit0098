fc9a3d85d65089a3115af953b528a4161ef887f5
ashmitgupta1607-stack <ashmitgupta1607@gmail.com>
2025-08-20 22:20:13 +0530

    Add files via upload

---
+++ code.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int x=0,y=0,z=0 , vote;
+    printf("1 for candidate x\n");
+    printf("2 for candidate y\n");
+    printf("3 for candidate z\n");
+    printf("enter your number\n");
+    scanf("%d",& vote);
+    switch (vote)
+    {
+    case 1:
+        x=x+1;
+        break;
+    case 2:
+    y=y+1;
+        break;
+    case 3:
+        z=z+1;
+        break;
+    }
+printf("voting results :\n");
+printf("candidate x :%d",x);
+printf("\ncandidate y :%d",y);
+printf("candidate z :%d",z);
+return 0;
+
+
+}
